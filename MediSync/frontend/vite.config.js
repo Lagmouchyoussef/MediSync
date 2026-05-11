4959752eab54df047879c66c192cd9b1d1fb5a89
Lagmouchyoussef <yousseflagmouxch@gmail.com>
2026-05-11 15:18:34 +0100

    new

--- MediSync/frontend/vite.config.js
+++ MediSync/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/static/' : '/',
   server: {
     port: 3000,
     proxy: {
