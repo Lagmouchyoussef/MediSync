4a57a899a3d6533f2e5184014fd44622fafff2d4
Lagmouchyoussef <yousseflagmouxch@gmail.com>
2026-05-11 15:29:03 +0100

    new

--- MediSync/frontend/src/modules/auth/Login.jsx
+++ MediSync/frontend/src/modules/auth/Login.jsx
@@ -82,7 +82,7 @@ export default function Login() {
         playsInline
         preload="auto"
       >
-        <source src={`${import.meta.env.BASE_URL}background.mp4`} type="video/mp4" />
+        <source src="background.mp4" type="video/mp4" />
       </video>
 
       <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-10"></div>
